d4c1056b303f3f13aa004523981f5fa3893321a9
Cameron Little <cameron@camlittle.com>
2020-10-03 23:26:05 +0200

    Fix build issues with utils

--- rollup.config.main.js
+++ rollup.config.main.js
@@ -1,4 +1,5 @@
 import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 
 // NOTE: this works, but has a couple issues.
@@ -14,7 +15,7 @@ import resolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/main.ts",
-  plugins: [typescript(), resolve()],
+  plugins: [typescript(), commonjs(), resolve()],
   output: {
     file: "json.novaextension/Scripts/main.dist.js",
     sourcemap: true,
